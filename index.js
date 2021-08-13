79af9d0d4e3ab86c315b9b45de4002bc135666b4
SmeraldaKa0s <anabella.lerner@gmail.com>
2021-08-13 02:57:09 -0300

    agregando estilos en css y eventos

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// dark & light mode
+// Dark & Light mode
 
 const darkMode = document.querySelector(".dark-mode")
 const buttonMode = document.querySelector(".button-mode")
@@ -8,22 +8,45 @@ buttonMode.onclick = () => {
     darkMode.classList.toggle("dark-mode")
     darkMode.classList.toggle("light-mode")
 
-    if (darkMode.classList.contains("dark-mode")){
+    if (darkMode.classList.contains("dark-mode")) {
         buttonMode.textContent = "🌞 Light mode"
     }
-    else{
+    else {
         buttonMode.textContent = "🌙 Dark mode"
     }
 }
 
-// dowload meme
+// Download Meme
 
 const downloadMeme = document.getElementById("download-container");
 const downloadButton = document.getElementById("download-button");
 
 downloadButton.onclick = () => {
-  domtoimage.toBlob(downloadMeme)
-  .then(function (blob) {
-      window.saveAs(blob, 'Your Meme.png');
-  })
+    domtoimage.toBlob(downloadMeme)
+        .then(function (blob) {
+            window.saveAs(blob, 'Your_Meme.png');
+        })
 }
+
+
+// Image Panel 
+
+const inputUrl = document.getElementById("input-url");
+const imageContainer = document.getElementById("image-container");
+
+inputUrl.oninput = () => {
+    imageContainer.src = inputUrl.value
+}
+
+
+
+// Filters
+
+const brightness = document.getElementById("brightness");
+console.log(brightness)
+
+const handleChange = (event) => {
+    console.log(event.target.value)
+}
+
+brightness.addEventListener("change", handleChange)
